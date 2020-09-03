115a2061fd871d8991be2ba9ceffface78c1e6e4
alex-diachenko <dyachenko664@gmail.com>
2020-09-03 17:00:47 +0300

    created requests to firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import './Auth.css'
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import is from 'is_js'
+import axios from 'axios';
 
 export default class Auth extends Component {
 
@@ -36,8 +37,34 @@ export default class Auth extends Component {
     }
   };
   
-  loginHandler = () => {
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC2y5nZOsKZLS42_rPg3SaFLDybzIlLHuw', authData)
+      console.log("Auth -> registerHandler -> response", response)
+      
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC2y5nZOsKZLS42_rPg3SaFLDybzIlLHuw', authData)
+      console.log("Auth -> registerHandler -> response", response)
+      
+    } catch (e) {
+      console.log(e);
+    }
   }
   
   submitHandler = event => {

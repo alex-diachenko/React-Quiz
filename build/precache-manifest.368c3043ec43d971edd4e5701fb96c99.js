8957eb75c221daa08db90b44f4ad2f6dc3c3b147
alex-diachenko <dyachenko664@gmail.com>
2020-09-11 10:24:33 +0300

    changed app work with redux, deployed to firebase

---
+++ build/precache-manifest.368c3043ec43d971edd4e5701fb96c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b7655346962000ae3cbbdce2654c61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dc0220f485c93f59d41",
+    "url": "/static/css/main.2b1051bd.chunk.css"
+  },
+  {
+    "revision": "b7c08a9e7e4e1779b46f",
+    "url": "/static/js/2.c396688d.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/static/js/2.c396688d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dc0220f485c93f59d41",
+    "url": "/static/js/main.50e08203.chunk.js"
+  },
+  {
+    "revision": "0cead240beaeac72778b",
+    "url": "/static/js/runtime-main.50b5716e.js"
+  }
+]);
